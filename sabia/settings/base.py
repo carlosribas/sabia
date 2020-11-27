790046625616558f491890f08b8cc1754d9cb3b2
carlosribas <caduribas@gmail.com>
2020-11-27 21:38:03 +0000

    Enable login with Facebook

--- sabia/settings/base.py
+++ sabia/settings/base.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'widget_tweaks',
     'userauth',
     'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.facebook',
 ]
 
 MIDDLEWARE = [
@@ -218,4 +219,25 @@ SOCIALACCOUNT_PROVIDERS = {
             'access_type': 'online',
         },
     },
+    'facebook': {
+        'METHOD': 'oauth2',
+        'SDK_URL': '//connect.facebook.net/{locale}/sdk.js',
+        'SCOPE': ['email', 'public_profile'],
+        'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
+        'INIT_PARAMS': {'cookie': True},
+        'FIELDS': [
+            'id',
+            'first_name',
+            'last_name',
+            'middle_name',
+            'name',
+            'name_format',
+            'picture',
+            'short_name'
+        ],
+        'EXCHANGE_TOKEN': True,
+        'LOCALE_FUNC': 'path.to.callable',
+        'VERIFIED_EMAIL': False,
+        'VERSION': 'v7.0',
+    }
 }
