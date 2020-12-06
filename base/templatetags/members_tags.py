9376372a39530f7fbf96ddb329f3832cf1f145f3
carlosribas <caduribas@gmail.com>
2020-12-06 23:41:15 +0000

    Team page

---
+++ base/templatetags/members_tags.py
@@ -0,0 +1,14 @@
+from django import template
+
+from base.models import TeamMember
+
+register = template.Library()
+
+
+@register.inclusion_tag('base/members_page.html', takes_context=True)
+def get_members(context):
+    members = TeamMember.objects.all()
+
+    return {
+        'members': members,
+    }
