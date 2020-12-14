13ed9c8099d39be1cdc95e90d6591c9b26e84099
carlosribas <caduribas@gmail.com>
2020-12-14 17:30:25 +0000

    Change size

--- base/image_formats.py
+++ base/image_formats.py
@@ -4,7 +4,7 @@ unregister_image_format('fullwidth')
 register_image_format(Format('fullwidth', 'Full width', 'richtext-image img-fluid', 'width-1100'))
 
 unregister_image_format('left')
-register_image_format(Format('left', 'Left-aligned', 'richtext-image img-fluid float-left mr-3', 'width-500'))
+register_image_format(Format('left', 'Left-aligned', 'richtext-image img-fluid float-left mr-3', 'width-450'))
 
 unregister_image_format('right')
-register_image_format(Format('right', 'Right-aligned', 'richtext-image img-fluid float-right ml-3', 'width-500'))
+register_image_format(Format('right', 'Right-aligned', 'richtext-image img-fluid float-right ml-3', 'width-450'))
