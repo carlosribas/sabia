0078337deb0146dcfadafc5fee89c86c618a8695
carlosribas <caduribas@gmail.com>
2020-12-14 15:46:15 +0000

    Create file to add bootstrap classes to images

---
+++ base/image_formats.py
@@ -0,0 +1,10 @@
+from wagtail.images.formats import Format, register_image_format, unregister_image_format
+
+unregister_image_format('fullwidth')
+register_image_format(Format('fullwidth', 'Full width', 'richtext-image img-fluid', 'width-1100'))
+
+unregister_image_format('left')
+register_image_format(Format('left', 'Left-aligned', 'richtext-image img-fluid float-left mr-3', 'width-500'))
+
+unregister_image_format('right')
+register_image_format(Format('right', 'Right-aligned', 'richtext-image img-fluid float-right ml-3', 'width-500'))
