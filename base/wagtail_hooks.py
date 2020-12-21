806582132c1f3ad77575120bd4a3043bca33ab09
carlosribas <caduribas@gmail.com>
2020-12-21 11:47:46 +0000

    Hide snippets from menu

--- base/wagtail_hooks.py
+++ base/wagtail_hooks.py
@@ -1,12 +1,19 @@
-from wagtail.contrib.modeladmin.options import (
-    ModelAdmin, ModelAdminGroup, modeladmin_register)
+from django.utils.translation import ugettext as _
 
-from base.models import FooterText, TeamMember
+from wagtail.core import hooks
+from wagtail.contrib.modeladmin.options import (ModelAdmin, ModelAdminGroup, modeladmin_register)
+
+from base.models import FooterText, Menu, TeamMember
+
+
+@hooks.register('construct_main_menu')
+def hide_snippets_menu_item(request, menu_items):
+    menu_items[:] = [item for item in menu_items if item.name != 'fragmentos']
 
 
 class TeamMemberModelAdmin(ModelAdmin):
     model = TeamMember
-    menu_label = 'Team'
+    menu_label = _('Team')
     menu_icon = 'fa-users'
     list_display = ('name', 'job_title', 'thumb_image')
     list_filter = ('job_title', )
@@ -15,14 +22,23 @@ class TeamMemberModelAdmin(ModelAdmin):
 
 class FooterTextAdmin(ModelAdmin):
     model = FooterText
+    menu_label = _('Footer Text')
+    menu_icon = 'fa-pencil'
     search_fields = ('body',)
 
 
+class MenuAdmin(ModelAdmin):
+    model = Menu
+    menu_label = _('Menu')
+    menu_icon = 'fa-bars'
+    search_fields = ('title', 'slug')
+
+
 class SabiaModelAdminGroup(ModelAdminGroup):
-    menu_label = 'General'
+    menu_label = _('General')
     menu_icon = 'cog'
     menu_order = 300  # will put in 4th place (000 being 1st, 100 2nd)
-    items = (FooterTextAdmin, TeamMemberModelAdmin)
+    items = (FooterTextAdmin, MenuAdmin, TeamMemberModelAdmin)
 
 
 # When using a ModelAdminGroup class to group several ModelAdmin classes together,
