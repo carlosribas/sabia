02adfe088da73b36da373fcf289ab2e263f8d1ad
carlosribas <caduribas@gmail.com>
2020-12-23 11:50:07 +0000

    New carousel

---
+++ home/static/home/js/carousel.js
@@ -0,0 +1,54 @@
+// TEAM CARROSEL
+const slideBox = $('#teambox .container-team');
+const teamNodes = [];
+
+$.each($('.person'), (index, el) => teamNodes.push(el));
+
+const cardSize = $('.person').width();
+const firstSlide = 0;
+
+const btnNext = $('#btnNext');
+const btnPrev = $('#btnPrev');
+
+// handle infinite next slides
+function nextMember() {
+    console.log('next');
+    $(teamNodes[firstSlide]).children('.title').animate({
+        opacity: 0
+    }, 200);
+
+    $(teamNodes[firstSlide]).animate({
+        width: 0,
+    }, 400, function() {
+        $(teamNodes[firstSlide]).children('.title').animate({ opacity: 1});
+        // remove dom element
+        $(teamNodes[firstSlide]).remove();
+
+        // change element position for last of array
+        const el = teamNodes.shift();
+        teamNodes.push(el);
+        // put last node in DOM again
+        $(teamNodes[teamNodes.length - 1]).css('width', cardSize);
+        slideBox.append(teamNodes[teamNodes.length - 1]);
+
+    });
+};
+
+// handle infinite previos slides
+function prevMember() {
+    $(teamNodes[teamNodes.length - 1]).css('width', 0);
+
+    $(teamNodes[teamNodes.length - 1]).remove();
+
+    const el = teamNodes.pop();
+    teamNodes.unshift(el);
+    slideBox.prepend(teamNodes[firstSlide]);
+    $(teamNodes[firstSlide]).animate({
+        width: cardSize,
+    }, 400, function() {
+
+    });
+};
+
+btnNext.click(() => nextMember());
+btnPrev.click(() => prevMember());
