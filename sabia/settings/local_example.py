efaf666ffd6c4fa99309fe7c13f3132c1f24e61d
carlosribas <caduribas@gmail.com>
2021-01-30 19:54:16 +0000

    Example of local.py file

---
+++ sabia/settings/local_example.py
@@ -0,0 +1,24 @@
+SECRET_KEY = 'your_secret_key'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sabia',
+        'USER': 'sabia',
+        'PASSWORD': 'sabia',
+        'HOST': 'db',
+    }
+}
+
+EMAIL_HOST = 'your.host.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'email@example.com'
+EMAIL_HOST_PASSWORD = 'password'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+# reCaptcha settings
+RECAPTCHA_PUBLIC_KEY = 'reCaptcha_public_key'
+RECAPTCHA_PRIVATE_KEY = 'reCaptcha_private_key'
+# enable no captcha
+NOCAPTCHA = True
