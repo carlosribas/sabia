760f1783825046041f14fd644a748a1897562e1a
carlosribas <caduribas@gmail.com>
2022-10-28 19:44:52 +0100

    Set password for local development

--- init.sql
+++ init.sql
@@ -1,3 +1,4 @@
-CREATE USER sabia WITH SUPERUSER;
+-- to be used for local development
+CREATE USER sabia SUPERUSER PASSWORD 'sabia';
 CREATE DATABASE sabia;
 GRANT ALL PRIVILEGES ON DATABASE sabia TO sabia;
