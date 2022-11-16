7872c651483f7f43e779601594d1ba5abd002110
caco <cacods21@gmail.com>
2022-11-16 16:27:15 -0300

    Extract Mercado Pago javascript to a file

---
+++ base/static/base/js/mercadopago.js
@@ -0,0 +1,20 @@
+const initMercadoPagoButton = function (pubKey, prevenceId) {
+    const mp = new MercadoPago(pubKey, {
+        locale: 'pt-BR'
+    });
+
+    mp.checkout({
+        preference: {
+            id: prevenceId
+        },
+        render: {
+            container: '.cho-container',
+            label: 'Pagar',
+        }
+    });
+}
+
+// TODO: why $('#mp_button button').click doesn't work?
+$("#mp_button").click(function (e) {
+    e.preventDefault();
+})
