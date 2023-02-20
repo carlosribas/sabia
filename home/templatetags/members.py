eb0472200d28d9552bb1ae88dbfc42862e20fc18
carlosribas <caduribas@gmail.com>
2023-02-20 23:06:14 +0000

    Change order

--- home/templatetags/members.py
+++ home/templatetags/members.py
@@ -20,8 +20,8 @@ def get_members(context):
 @register.inclusion_tag('home/courses.html', takes_context=True)
 def get_courses(context):
     today = datetime.datetime.today()
-    next_courses = Course.objects.filter(start_date__gte=today).exclude(type='admin').order_by('-start_date')
-    other_courses = Course.objects.exclude(Q(start_date__gte=today) | Q(type='admin')).order_by('-start_date')
+    next_courses = Course.objects.filter(start_date__gte=today).exclude(type='admin').order_by('start_date')
+    other_courses = Course.objects.exclude(Q(start_date__gte=today) | Q(type='admin')).order_by('start_date')
     courses = list(chain(next_courses, other_courses))[:3]
     hidden_courses = Course.objects.filter(type='admin')
     return {
